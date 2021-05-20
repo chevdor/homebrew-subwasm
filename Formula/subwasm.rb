ea6b6dbec614481c611f2e4a363c72354798c44b
chevdor <chevdor@gmail.com>
2021-05-20 21:01:06 +0200

    Release v0.8.0

--- Formula/subwasm.rb
+++ Formula/subwasm.rb
@@ -4,9 +4,9 @@
 class Subwasm < Formula
   desc "CLI utility to get information about Substrate based chains Runtime WASM"
   homepage "https://gitlab.com/chevdor/subwasm"
-  url "https://github.com/chevdor/homebrew-subwasm/releases/download/v0.7.0/subwasm-mac-v0.7.0.tar.gz"
-  sha256 "dd594abae197ea0156b230f94d71cd537295c3ba5a9054fc3b987f7b05f3696d"
-  version "0.7.0"
+  url "https://github.com/chevdor/homebrew-subwasm/releases/download/v0.8.0/subwasm-mac-v0.8.0.tar.gz"
+  sha256 "1f27dbc13935d418362fe2c0cef38e0a5a340eb3ab6ac8dd1c9a4ee069846f35"
+  version "0.8.0"
 
   def install
     bin.install "subwasm"
